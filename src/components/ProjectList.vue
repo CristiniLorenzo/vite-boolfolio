e40cab1bc06fc03379e96c88e489cc0ca9f05b04
CristiniLorenzo <lorenzo.cristini98@gmail.com>
2024-06-12 17:13:41 +0200

    first commit

---
+++ src/components/ProjectList.vue
@@ -0,0 +1,14 @@
+<script>
+export default {
+    name: 'ProjectList'
+}
+</script>
+
+<template>
+    <div class="container">
+        <h1>ciaoooo</h1>
+    </div>
+</template>
+
+<style scoped lang="scss">
+</style>
