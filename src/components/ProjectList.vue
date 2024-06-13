a0103f134f39c183b06f6bc2fa41c781245440a7
CristiniLorenzo <lorenzo.cristini98@gmail.com>
2024-06-13 02:27:12 +0200

    make api call and showed api content in cards

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,12 +1,46 @@
 <script>
+import axios from 'axios';
+
 export default {
-    name: 'ProjectList'
+    name: 'ProjectList',
+    data() {
+        return {
+            projects: []
+        };
+    },
+    methods: {
+        getProjects() {
+            axios.get('http://127.0.0.1:8000/api/projects')
+            .then((response) => {
+                this.projects = response.data.results;
+            });
+        }
+    },
+    mounted() {
+        this.getProjects();
+    }
 }
 </script>
 
 <template>
     <div class="container">
-        <h1>ciaoooo</h1>
+        <h1>All Projects</h1>
+
+        <div class="row row-cols-4">
+            <div v-for="project in projects" class="col">
+
+                <div class="card my-2">
+                    <!-- <img src="..." class="card-img-top" alt="..."> -->
+                    <div class="card-body">
+                        <h5 class="card-title">{{ project.name }}</h5>
+                        <div class="card-text my-3"><strong>Client name:</strong> {{ project.client_name }}</div>
+                        <p v-if="project.summary" class="card-text">{{ project.summary }}</p>
+                        <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
 </template>
 
