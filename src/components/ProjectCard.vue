da0741901db2c5fa8a22d0f46ef9d31b27c60769
CristiniLorenzo <lorenzo.cristini98@gmail.com>
2024-06-13 03:12:18 +0200

    showed types and technologies in cards

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -13,8 +13,21 @@ export default {
             <!-- <img src="..." class="card-img-top" alt="..."> -->
             <div class="card-body">
                 <h5 class="card-title">{{ projectInfo.name }}</h5>
-                <div class="card-text my-3"><strong>Client name:</strong> {{ projectInfo.client_name }}</div>
-                <p v-if="projectInfo.summary" class="card-text">{{ projectInfo.summary }}</p>
+                <div class="card-text my-3">
+                    <strong>Client name:</strong> {{ projectInfo.client_name }}
+                </div>
+                
+                <div class="card-text my-3" v-if="projectInfo.type">
+                    <strong>Type:</strong> {{ projectInfo.type.name }}
+                </div>
+
+                <div class="card-text my-3" v-if="projectInfo.technologies.length > 0">
+                    <strong>Technologies: </strong><span v-for="technology in projectInfo.technologies">{{ technology.name }} &nbsp;</span>
+                </div>
+
+                <p v-if="projectInfo.summary" class="card-text">
+                    {{ projectInfo.summary }}
+                </p>
                 <!-- <a href="#" class="btn btn-primary">Go somewhere</a> -->
             </div>
         </div>
